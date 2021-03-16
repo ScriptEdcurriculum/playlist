45a43dd21d9241f31617da81138cb0aa6ed598c4
Glitch (plsylist-ste) <none>
2021-03-16 01:00:17 +0000

    🚨🚋 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -8,24 +8,60 @@ let storeLink = ["https://www.youtube.com/watch?v=yydNF8tuVmU&ab_channel=DaftPun
 let storeImage=["https://media.pitchfork.com/photos/6033fa293221fafe5fc2d1ad/2:1/w_2914,h_1457,c_limit/Daft-Punk.jpg",
                 "https://m.media-amazon.com/images/I/71XV4iaETAL._SS500_.jpg"]
 
-for(let Song of storeSong){
-  $("#songs").append(`<p>${Song}</p>`);
-  
-}
-for (let Artist of storeArtist){
-  $("#artist").append(`<p>${Artist}</p>`);
-}
 
-for(let Length of storeLength){
-  $("#length").append(`<p>${Length}</p>`);
-  
-}
 
-for (let Image of storeImage){
-  $("#image").append(` <img src="${Image}">`);
-}
-
-for(let Link of storeLink){
-  $("#link").append(`<p><a href ="${Link}">Link here </a></p>`);
+$("button").click(function() {
+  let songName = $(".Song").val();
+  console.log(songName); 
+  storeSong.push(songName);
+  console.log(storeSong); 
   
-}
+  let songArtist = $(".Artist").val();
+  console.log(songArtist); 
+  storeArtist.push(songArtist);
+  console.log(storeArtist); 
+  
+  let songLength= $(".Length").val();
+  console.log(songLength);
+  storeLength.push(songLength);
+  console.log(storeLength);
+  
+  let songLink= $(".Link").val();
+  console.log(songLink);
+  storeLink.push(songLink);
+  console.log(storeLink);
+
+
+  let songImg = $(".Image").val();
+  console.log(songImg); 
+  storeImage.push(songImg);
+  console.log(storeImage); 
+  
+   $(".column").empty();
+  
+  for(let Song of storeSong){
+    $("#songs").append(`<p>${Song}</p>`);
+
+  }
+  for (let Artist of storeArtist){
+    $("#artist").append(`<p>${Artist}</p>`);
+  }
+
+  for(let Length of storeLength){
+    $("#length").append(`<p>${Length}</p>`);
+
+  }
+
+  for (let Image of storeImage){
+    $("#image").append(` <img src="${Image}">`);
+  }
+
+  for(let Link of storeLink){
+    $("#link").append(`<p><a href ="${Link}">Link here </a></p>`);
+
+  }
+  
+  
+});
+
+
